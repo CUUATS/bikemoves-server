e0fc885463864774a9d0bfaa9a150400e36b91c4
Matt Yoder <myoder@ccrpc.org>
2016-02-02 15:06:58 -0600

    Simple node app and dockerfile.

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM node:4-onbuild
+
+VOLUME /var/bikemoves
+EXPOSE 8888
---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var lzString = require('lz-string');
+var app = express();
+
+var file_path = '/var/bikemoves/trips.json';
+
+app.enable('trust proxy');
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.post('/v0.1/trip', function(req, res) {
+  var body = req.body;
+  if (body.tripData) {
+    var data = JSON.parse(lzString.decompressFromBase64(body.tripData));
+    fs.appendFile(file_path, JSON.stringify(data, null, 2));
+  }
+  res.send('Trip saved');
+});
+
+app.listen(8888);

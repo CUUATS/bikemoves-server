747b51fd3909f93ca30cc95e845651605277819e
dfu13 <dfu.lfhs@gmail.com>
2016-02-22 18:50:39 -0600

    undoing stuff

--- app.js
+++ app.js
@@ -18,16 +18,17 @@ app.use(function(req, res, next) {
 app.use(bodyParser.json());
 
 app.post('/v0.1/trip', function(req, res) {
-  /*var body = req.body;
+  var body = req.body;
   fs.exists(file_path, function(exists) {
     if(!exists){
       fs.mkdir(file_path, function(error){});
     }
   });
   if (body.tripData) {
+    //var data = JSON.parse(lzString.decompressFromBase64(body.tripData));
     var data = JSON.parse(body.tripData);
     fs.appendFile(file_path, JSON.stringify(data, null, 2));
-  }*/
+  }
   res.send('Trip saved');
 });
 

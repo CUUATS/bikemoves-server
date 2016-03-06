2821be882fd485a95e52bb7c602ec0d233e40c34
dfu13 <dfu.lfhs@gmail.com>
2016-03-06 17:21:21 -0600

    Creates databases

--- app.js
+++ app.js
@@ -2,9 +2,88 @@ var express = require('express');
 var fs = require('fs');
 var bodyParser = require('body-parser');
 var lzString = require('lz-string');
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('bikemoves.db');
 var app = express();
 
 var file_path = '/var/bikemoves/trips.json';
+
+db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='User'",
+       function(err, rows) {
+  if(err !== null) {
+    console.log(err);
+  }
+  else if(rows === undefined) {
+    db.run('CREATE TABLE "User" ' +
+           '("id" INTEGER PRIMARY KEY AUTOINCREMENT, ' +
+           '"device_uuid" VARCHAR(255), ' +
+           '"gender" CHARACTER, ' +
+           '"age" INTEGER, ' +
+           '"cycling_experience" VARCHAR(255))', function(err) {
+      if(err !== null) {
+        console.log(err);
+      }
+      else {
+        console.log("SQL Table 'User' initialized.");
+      }
+    });
+  }
+  else {
+    console.log("SQL Table 'User' already initialized.");
+  }
+});
+
+db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='Trip'",
+       function(err, rows) {
+  if(err !== null) {
+    console.log(err);
+  }
+  else if(rows === undefined) {
+    db.run('CREATE TABLE "Trip" ' +
+           '("id" INTEGER PRIMARY KEY AUTOINCREMENT, ' +
+           '"user_id" INTEGER, ' +
+           '"origin_type" VARCHAR(255), ' +
+           '"destination_type" VARCHAR(255), ' +
+           '"start_datetime" TIMESTAMP, ' +
+           '"end_datetime" TIMESTAMP, ' +
+           '"trip_geom" VARCHAR(255))', function(err) {
+      if(err !== null) {
+        console.log(err);
+      }
+      else {
+        console.log("SQL Table 'Trip' initialized.");
+      }
+    });
+  }
+  else {
+    console.log("SQL Table 'Trip' already initialized.");
+  }
+});
+
+db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='Trip_point'",
+       function(err, rows) {
+  if(err !== null) {
+    console.log(err);
+  }
+  else if(rows === undefined) {
+    db.run('CREATE TABLE "Trip_point" ' +
+           '("id" INTEGER PRIMARY KEY AUTOINCREMENT, ' +
+           '"trip_id" INTEGER, ' +
+           '"datetime" TIMESTAMP, ' +
+           '"gps_accuracy" FLOAT, ' +
+           '"geom" VARCHAR(255))', function(err) {
+      if(err !== null) {
+        console.log(err);
+      }
+      else {
+        console.log("SQL Table 'User' initialized.");
+      }
+    });
+  }
+  else {
+    console.log("SQL Table 'User' already initialized.");
+  }
+});
 
 app.enable('trust proxy');
 
@@ -25,8 +104,8 @@ app.post('/v0.1/trip', function(req, res) {
     }
   });
   if (body.tripData) {
-    //var data = JSON.parse(lzString.decompressFromBase64(body.tripData));
-    var data = JSON.parse(body.tripData);
+    var data = JSON.parse(lzString.decompressFromBase64(body.tripData));
+    //var data = JSON.parse(body.tripData);
     fs.appendFile(file_path, JSON.stringify(data, null, 2));
   }
   res.send('Trip saved');

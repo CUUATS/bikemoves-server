53f14601ba41f1939c504a7847d595f84e8edc88
Matt Yoder <myoder@ccrpc.org>
2017-09-07 14:33:29 -0500

    Use ES6/Babel.

---
+++ src/public/explore/explore.es6
@@ -0,0 +1,127 @@
+class Explore {
+  constructor() {
+    this.state = {
+      chartView: 'users'
+    };
+    this.initCharts();
+    // this.initMap();
+  }
+
+  initMap() {
+    this.map = map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/light-v9',
+        center: [-88.227203, 40.109403],
+        zoom: 13
+    });
+    map.on('load', this.addLayers.bind(this));
+  }
+
+  initCharts() {
+    this.getJSON(this.absoluteURL('/demographics.json'))
+      .then((data) => {
+        console.log(data);
+        this.initChartViews(data);
+      });
+  }
+
+  initChartViews(data) {
+    let viewButtons = document.querySelectorAll('#stats li');
+    viewButtons.forEach((button) => {
+      let link = button.querySelector('a'),
+        value = button.querySelector('.value'),
+        statName = button.className;
+
+      value.innerHTML = this.formatNumber(this.getStatTotal(data, statName), 0);
+      link.addEventListener('click', (e) => {
+        e.preventDefault();
+        this.showStatCharts(statName);
+      });
+      if (statName === this.state.chartView) this.showStatCharts(statName);
+    });
+  }
+
+  showStatCharts(statName) {
+    document.querySelectorAll('#stats li a').forEach((link) => {
+      link.className = (link.parentNode.className === statName) ? 'active' : '';
+    });
+
+  }
+
+  getStatTotal(data, statName) {
+    return data.gender.reduce((sum, row) => {
+      return sum + row[statName];
+    }, 0);
+  }
+
+  formatNumber(value, digits) {
+    return (+value.toFixed(digits)).toLocaleString();
+  }
+
+  absoluteURL(url) {
+    return location.protocol + '//' + location.hostname + (
+      location.port ? ':' + location.port : '') + url;
+  }
+
+  getJSON(url) {
+    return new Promise((resolve, reject) => {
+      let req = new XMLHttpRequest();
+      req.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200)
+          resolve(JSON.parse(this.responseText));
+      };
+      req.open('GET', url, true);
+      req.send();
+    });
+  }
+
+  addLayers() {
+    this.map.addSource('explore', {
+      type: 'vector',
+      tilejson: '2.2.0',
+      name: 'bikemoves-explore',
+      description: 'Map data for the BikeMoves Explore web app.',
+      version: '1.0.0',
+      attribution: '<a href="https://ccrpc.org/">&copy; CCRPC</a>',
+      scheme: 'xyz',
+      tiles: [
+         this.absoluteURL('/explore/{z}/{x}/{y}.mvt')
+      ]
+    });
+
+    this.map.addLayer({
+      id: 'bikemoves-edge',
+      type: 'line',
+      source: 'explore',
+      'source-layer': 'edge',
+      paint: {
+        'line-color': {
+          type: 'interval',
+          property: 'mean_speed',
+          stops: [
+            [1, '#d7191c'],
+            [2, '#fdae61'],
+            [3, '#ffffbf'],
+            [4, '#abd9e9'],
+            [5, '#2c7bb6']
+          ]
+        },
+        'line-width': {
+          type: 'interval',
+          property: 'users',
+          stops: [
+            [2, 4],
+            [4, 8],
+            [6, 12],
+            [8, 16]
+          ]
+        }
+      },
+      layout: {
+        'line-cap': 'round'
+      }
+    }, 'road-label-small');
+  }
+}
+
+let explore = new Explore();
--- src/public/explore/explore.js
+++ src/public/explore/explore.js
@@ -1,124 +1,135 @@
-var Explore = function() {
-  this.state = {
-    chartView: 'users'
-  };
-  this.initCharts();
-  // this.initMap();
-};
+'use strict';
 
-Explore.prototype.initMap = function() {
-  this.map = map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/light-v9',
-      center: [-88.227203, 40.109403],
-      zoom: 13
-  });
-  map.on('load', this.addLayers.bind(this));
-};
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-Explore.prototype.initCharts = function() {
-  var ex = this;
-  this.getJSON(this.absoluteURL('/demographics.json'), function(data) {
-    console.log(data);
-    ex.initChartViews(data);
-  });
-};
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-Explore.prototype.initChartViews = function(data) {
-  var ex = this,
-    viewButtons = document.querySelectorAll('#stats li');
-  viewButtons.forEach(function(button) {
-    var link = button.querySelector('a'),
-      value = button.querySelector('.value'),
-      statName = button.className;
+var Explore = function () {
+  function Explore() {
+    _classCallCheck(this, Explore);
 
-    value.innerHTML = ex.formatNumber(ex.getStatTotal(data, statName), 0);
-    link.addEventListener('click', function(e) {
-      e.preventDefault();
-      ex.showStatCharts(this.parentNode.className);
-    });
-    if (statName === ex.state.chartView) ex.showStatCharts(statName);
-  });
-};
+    this.state = {
+      chartView: 'users'
+    };
+    this.initCharts();
+    // this.initMap();
+  }
 
-Explore.prototype.showStatCharts = function(statName) {
-  document.querySelectorAll('#stats li a').forEach(function(link) {
-    link.className = (link.parentNode.className === statName) ? 'active' : '';
-  });
-
-};
-
-Explore.prototype.getStatTotal = function(data, statName) {
-  return data.gender.reduce(function(sum, row) {
-    return sum + row[statName];
-  }, 0);
-};
-
-Explore.prototype.formatNumber = function(value, digits) {
-  return (+value.toFixed(digits)).toLocaleString();
-};
-
-Explore.prototype.absoluteURL = function(url) {
-  return location.protocol + '//' + location.hostname + (
-    location.port ? ':' + location.port : '') + url;
-};
-
-Explore.prototype.getJSON = function(url, callback) {
-  var req = new XMLHttpRequest();
-  req.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200)
-      callback(JSON.parse(this.responseText));
-  };
-  req.open('GET', url, true);
-  req.send();
-};
-
-Explore.prototype.addLayers = function() {
-  this.map.addSource('explore', {
-    type: 'vector',
-    tilejson: '2.2.0',
-    name: 'bikemoves-explore',
-    description: 'Map data for the BikeMoves Explore web app.',
-    version: '1.0.0',
-    attribution: '<a href="https://ccrpc.org/">&copy; CCRPC</a>',
-    scheme: 'xyz',
-    tiles: [
-       this.absoluteURL('/explore/{z}/{x}/{y}.mvt')
-    ]
-  });
-
-  this.map.addLayer({
-    id: 'bikemoves-edge',
-    type: 'line',
-    source: 'explore',
-    'source-layer': 'edge',
-    paint: {
-      'line-color': {
-        type: 'interval',
-        property: 'mean_speed',
-        stops: [
-          [1, '#d7191c'],
-          [2, '#fdae61'],
-          [3, '#ffffbf'],
-          [4, '#abd9e9'],
-          [5, '#2c7bb6']
-        ]
-      },
-      'line-width': {
-        type: 'interval',
-        property: 'users',
-        stops: [
-          [2, 4],
-          [4, 8],
-          [6, 12],
-          [8, 16]
-        ]
-      }
-    },
-    layout: {
-      'line-cap': 'round'
+  _createClass(Explore, [{
+    key: 'initMap',
+    value: function initMap() {
+      this.map = map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/light-v9',
+        center: [-88.227203, 40.109403],
+        zoom: 13
+      });
+      map.on('load', this.addLayers.bind(this));
     }
-  }, 'road-label-small');
-};
+  }, {
+    key: 'initCharts',
+    value: function initCharts() {
+      var _this = this;
+
+      this.getJSON(this.absoluteURL('/demographics.json')).then(function (data) {
+        console.log(data);
+        _this.initChartViews(data);
+      });
+    }
+  }, {
+    key: 'initChartViews',
+    value: function initChartViews(data) {
+      var _this2 = this;
+
+      var viewButtons = document.querySelectorAll('#stats li');
+      viewButtons.forEach(function (button) {
+        var link = button.querySelector('a'),
+            value = button.querySelector('.value'),
+            statName = button.className;
+
+        value.innerHTML = _this2.formatNumber(_this2.getStatTotal(data, statName), 0);
+        link.addEventListener('click', function (e) {
+          e.preventDefault();
+          _this2.showStatCharts(statName);
+        });
+        if (statName === _this2.state.chartView) _this2.showStatCharts(statName);
+      });
+    }
+  }, {
+    key: 'showStatCharts',
+    value: function showStatCharts(statName) {
+      document.querySelectorAll('#stats li a').forEach(function (link) {
+        link.className = link.parentNode.className === statName ? 'active' : '';
+      });
+    }
+  }, {
+    key: 'getStatTotal',
+    value: function getStatTotal(data, statName) {
+      return data.gender.reduce(function (sum, row) {
+        return sum + row[statName];
+      }, 0);
+    }
+  }, {
+    key: 'formatNumber',
+    value: function formatNumber(value, digits) {
+      return (+value.toFixed(digits)).toLocaleString();
+    }
+  }, {
+    key: 'absoluteURL',
+    value: function absoluteURL(url) {
+      return location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '') + url;
+    }
+  }, {
+    key: 'getJSON',
+    value: function getJSON(url) {
+      return new Promise(function (resolve, reject) {
+        var req = new XMLHttpRequest();
+        req.onreadystatechange = function () {
+          if (this.readyState == 4 && this.status == 200) resolve(JSON.parse(this.responseText));
+        };
+        req.open('GET', url, true);
+        req.send();
+      });
+    }
+  }, {
+    key: 'addLayers',
+    value: function addLayers() {
+      this.map.addSource('explore', {
+        type: 'vector',
+        tilejson: '2.2.0',
+        name: 'bikemoves-explore',
+        description: 'Map data for the BikeMoves Explore web app.',
+        version: '1.0.0',
+        attribution: '<a href="https://ccrpc.org/">&copy; CCRPC</a>',
+        scheme: 'xyz',
+        tiles: [this.absoluteURL('/explore/{z}/{x}/{y}.mvt')]
+      });
+
+      this.map.addLayer({
+        id: 'bikemoves-edge',
+        type: 'line',
+        source: 'explore',
+        'source-layer': 'edge',
+        paint: {
+          'line-color': {
+            type: 'interval',
+            property: 'mean_speed',
+            stops: [[1, '#d7191c'], [2, '#fdae61'], [3, '#ffffbf'], [4, '#abd9e9'], [5, '#2c7bb6']]
+          },
+          'line-width': {
+            type: 'interval',
+            property: 'users',
+            stops: [[2, 4], [4, 8], [6, 12], [8, 16]]
+          }
+        },
+        layout: {
+          'line-cap': 'round'
+        }
+      }, 'road-label-small');
+    }
+  }]);
+
+  return Explore;
+}();
 
 var explore = new Explore();
